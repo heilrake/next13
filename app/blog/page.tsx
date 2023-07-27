7b4ea464dc1f95fadb78ab90f6bd05f84a95adaf
hailrake <mishakvochko@ukr.net>
2023-07-27 14:15:55 +0200

    adds swr and posts

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,36 +1,17 @@
-import { usePosts } from "@/store";
-import Link from "next/link";
+// Components
+import { PostSearch } from "@/components/PostSearch";
+import { Posts } from "@/components/Posts";
 
 export const metaData = {
   title: Blog,
 };
 
-async function getData() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
-    next: {
-      revalidate: 60,
-    },
-  });
-  if (!response.ok) throw new Error("Unable to fetch posts");
-
-  return response.json();
-}
-
 export default async function Blog() {
-  const posts = await getData();
-
   return (
     <>
       <h2 className="font-black">Blog page</h2>
-      <ul>
-        {posts.map((post: any) => (
-          <li key={post.id}>
-            <Link className="underline text-sky-400" href={`/blog/${post.id}`}>
-              {post.title}{" "}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <PostSearch />
+      <Posts />
     </>
   );
 }
---
+++ components/PostSearch.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import useSWR from "swr";
+import { FormEventHandler, useState } from "react";
+import { getPostsBySearch } from "@/services/getPosts";
+
+export const PostSearch = () => {
+  const { mutate } = useSWR("posts");
+  const [search, setSearch] = useState<string>("");
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = async (event) => {
+    event.preventDefault();
+
+    const posts = await getPostsBySearch(search);
+    mutate(posts);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="search"
+        value={search}
+        onChange={(event) => setSearch(event.target.value)}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
